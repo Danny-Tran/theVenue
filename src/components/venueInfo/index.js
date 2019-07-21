54fe14272083c491727df14792a16282ba69cd97
Danny-Tran <truong_tran@live.com>
2019-07-21 20:50:01 +0000

    working on The Venue Info and location

---
+++ src/components/venueInfo/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import icon_calendar from '../../resources/images/icons/calendar.png'
+import icon_location from '../../resources/images/icons/location.png'
+
+const VenueInfo = () => {
+  return (
+    <div className='back_black'>
+      
+    </div>
+  );
+};
+
+export default VenueInfo;
