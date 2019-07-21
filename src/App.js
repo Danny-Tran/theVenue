e280edc4ee73dcdde4e84556bbbb94712b8702c4
Danny-Tran <truong_tran@live.com>
2019-07-21 21:28:50 +0000

    adding event information box

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './resources/styles.css'
 import Header from './components/header_footer/Header'
 import Featured from './components/feature/Featured'
-
+import VenueInfo from './components/venueInfo'
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height:'1500px', background:'lightblue'}}>
         <Header />
         <Featured />
+        <VenueInfo />
       </div>
     );
   }
