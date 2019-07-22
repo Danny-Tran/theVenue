a74e2704acffda6b3f440babf6b6049822b064ad
Danny-Tran <truong_tran@live.com>
2019-07-22 22:47:01 +0000

    pricing component complete

--- src/components/pricing/Price.js
+++ src/components/pricing/Price.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MyButton from '../util/MyButton'
+import Zoom from 'react-reveal/Zoom'
 
 class Price extends Component {
 
@@ -8,30 +10,37 @@ class Price extends Component {
     desc:[
       'Praesent commodo justo et leo fermentum, eu ultrices augue hendrerit. Donec porttitor justo sed arcu hendrerit',
       'Sed a sapien massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam a finibus risus, nec ullamcorper purus.',
-      'Aliquam maximus venenatis odio, vitae gravida ligula lacinia et. Integer consectetur est sed metus rutrum sollicitudin. Praesent volutpat cursus nisi, et lacinia tellus condimentum vel. Fusce lectus ex, condimentum nec mollis eu, elementum vel tortor.'
+      'Aliquam maximus venenatis odio, vitae gravida ligula lacinia et. Integer consectetur est sed metus rutrum sollicitudin. Praesent volutpat cursus nisi.'
     ],
-    linkto:['https://sales/b','https://sales/m','https://sales/s']
+    linkto:['https://sales/b','https://sales/m','https://sales/s'],
+    delay:[500,0,500]
   }
 
   showBoxes = () => (
     this.state.prices.map((box,i) =>(
-      <div className='pricing_item'> 
-        <div className='pricing_inner_wrapper'>
+      <Zoom delay={this.state.delay[i]} key={i}>
+        <div className='pricing_item'> 
+          <div className='pricing_inner_wrapper'>
+            <div className='pricing_title'>
+              <span>${this.state.prices[i]}</span>
+              <span>{this.state.positions[i]}</span>
+            </div>
 
-          <div className='pricing_title'>
-            <span>${this.state.prices[i]}</span>
+            <div className='pricing_description'>
+              <span>{this.state.desc[i]}</span>
+            </div>
+
+            <div className='pricing_buttons'>
+              <MyButton 
+                text='Purchase'
+                bck='#ffa800'
+                color='#ffffff'
+                link={this.state.linkto[i]}
+                />
+            </div>
           </div>
-
-          <div className='pricing_description'>
-            Description
-          </div>
-
-          <div className='pricing_buttons'>
-            button
-          </div>
-
         </div>
-      </div>
+      </Zoom>
     ))    
   )
 
